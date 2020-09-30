4751030d2e1e6bb09a505cbafb05cfef7106cbd9
Mohasalama <msalama2007@hotmail.com>
2020-09-30 11:27:40 -0400

    scrolling

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 // jQuery Smooth Scroll
 
-$('a').on('click', function(e){
-    if (this.hash !== ''){
-        e.preventDefault();
+// $('a').on('click', function(e){
+//     alert('yo');
+//     if (this.hash !== ''){
+//         e.preventDefault();
+        
+//         const hash = this.hash;
+//         $('html, body').animate({
+//             scrollTop: $(hash).offset().top
+//         }, 800);
+//         // 800 ms
 
-        const hash = this.hash;
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 800);
-        // 800 ms
-    }
-})
+        
+//         //when its clicked set the transform so not to affect other stuff
+//         // $('#page1').css({'transform' : 'translateY(-100%)'})
+
+//     }
+
+// })
 
 
